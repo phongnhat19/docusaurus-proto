0ec28c5537af828676717fe463de8bb70d4c1e58
TomokoMiyake <t.smile297327@gmail.com>
2020-04-20 01:42:01 +0900

    Add translation, kuc doc and versioning functions.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -7,6 +7,7 @@
 
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
+const RemarkablePlugins = require('./core/RemarkablePlugin');
 
 // List of projects/orgs using your project for the users page.
 const users = [
@@ -14,8 +15,8 @@ const users = [
     caption: 'User1',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: 'img/kuc_logo.png', //'/img/undraw_open_source.svg',
-    infoLink: 'https://tomokomiyake.github.io/docusaurus-proto/', //'https://www.facebook.com',
+    image: '/img/kuc_logo.png',
+    infoLink: 'https://tomokomiyake.github.io/docusaurus-proto/',
     pinned: true,
   },
 ];
@@ -23,28 +24,16 @@ const users = [
 const siteConfig = {
   title: 'kintone UI Component', // Title for your website.
   tagline: 'A reference website for kintone UI Component',
-  //url: 'https://your-docusaurus-test-site.com', // Your website URL
-  //baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
   url: 'https://tomokomiyake.github.io',
   baseUrl: '/docusaurus-proto/',
-
-  // Used for publishing and more
-  // projectName: 'test-site',
-  // organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
   projectName: 'docusaurus-proto',
   organizationName: 'tomokomiyake',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'getting-started', label: 'Docs'},
     {page: 'help', label: 'Help'},
+    {page: 'versions', label: 'Version'},
     {blog: true, label: 'Blog'},
     {search: true},
     {languages: true}
@@ -101,7 +90,19 @@ const siteConfig = {
   disableHeaderTitle: true,
   scrollToTop: true,
 
-  // Option for Algolia DocSearch
+  markdownPlugins: [
+    RemarkablePlugins.KUCComponentRenderer,
+  ],
+  scripts: [
+    'https://unpkg.com/@kintone/kintone-ui-component@0.3.3/dist/kintone-ui-component.min.js',
+    '/docusaurus-proto/js/extra.js'
+  ],
+  stylesheets: [
+    'https://unpkg.com/@kintone/kintone-ui-component@0.3.3/dist/kintone-ui-component.min.css',
+    '/docusaurus-proto/css/custom.css'
+  ]
+
+  // Option for Algolia DocSearch(It can be set at production-ready.)
   // algolia: {
   //   apiKey: '',
   //   indexName: ''
--- website/versioned_docs/version-0.1.0/getting-started.md
+++ website/versioned_docs/version-0.1.0/getting-started.md
@@ -1,14 +1,11 @@
 ---
-id: quickStart
-title: Quick Start
-sidebar_label: Quick Start
+id: version-0.1.0-getting-started
+title: Getting Started
+sidebar_label: Getting Started
+original_id: getting-started
 ---
 
 Check the [documentation](https://docusaurus.io) for how to use Docusaurus.
-
-## Lorem
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum massa eget nulla aliquet sagittis. Proin odio tortor, vulputate ut odio in, ultrices ultricies augue. Cras ornare ultrices lorem malesuada iaculis. Etiam sit amet libero tempor, pulvinar mauris sed, sollicitudin sapien.
 
 ## Mauris In Code
 
